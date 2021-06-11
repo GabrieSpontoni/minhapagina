1b87a62c87d305408dba557d6bc69dd63d8f4632
GabrieSpontoni <gabriel97_spontoni@hotmail.com>
2021-06-10 22:49:43 -0400

    configureBottonsSideBar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { useState } from 'react';
 
 function App() {
+  const [showSidebar, setShowSideBar] = useState(false);
+  const OpenSideBar = () => {
+    setShowSideBar (true)
+  }
+  const CloseSideBar = () => {
+    setShowSideBar(false)
+  }
+
   return (
     <div className="App">
       
-      <div className = 'App-hamburguer' alt = "">
-          <a href = '#'> <MenuIcon fontSize = 'large' style = {{color: '#FF5F6D'}}/></a>
+      <div onClick = {OpenSideBar} style = {{right: showSidebar?-40:40}} className = 'App-hamburguer'>
+          <a href> <MenuIcon fontSize = 'large' style = {{color: '#FF5F6D'}}/></a>
+      </div>
+      <div onClick = {CloseSideBar} style = {{right: showSidebar?40:-40}} className = 'App-close'>
+          <a href> <CloseIcon  fontSize = 'large' style = {{color: '#FF5F6D'}}/></a>
       </div>
 
 
@@ -27,22 +40,21 @@ function App() {
         
       </header>
 
-      <div className = 'App-sidebar'>
+      <div className = 'App-sidebar' style={{right: showSidebar?0:-400}} >
         <nav>
           <ul className = 'App-menu'>
-            <li className = 'App-menu-item'><a href = '#' className = 'App-menu-link'>Home</a> </li>
-            <li className = 'App-menu-item'><a href = '#' className = 'App-menu-link'>Conhecimentos</a> </li>
-            <li className = 'App-menu-item'><a href = '#' className = 'App-menu-link'>Projetos</a> </li>
-            <li className = 'App-menu-item'><a href = '#' className = 'App-menu-link'>Contato</a> </li>
-            <li className = 'App-menu-item'><a href = '#' className = 'App-menu-link'>Orçamento</a> </li>
+            <li className = 'App-menu-item'><a href className = 'App-menu-link'>Home</a> </li>
+            <li className = 'App-menu-item'><a href className = 'App-menu-link'>Conhecimentos</a> </li>
+            <li className = 'App-menu-item'><a href className = 'App-menu-link'>Projetos</a> </li>
+            <li className = 'App-menu-item'><a href className = 'App-menu-link'>Contato</a> </li>
+            <li className = 'App-menu-item'><a href className = 'App-menu-link'>Orçamento</a> </li>
           </ul>
         </nav>
         <div className = 'App-social-media'>
-          <a href = '#'> <FacebookIcon/></a>
-          <a href = '#'> <GitHubIcon/></a>
-          <a href = '#'> <TwitterIcon/></a>
+          <a href> <FacebookIcon fontSize = 'large'/></a>
+          <a href> <GitHubIcon   fontSize = 'large'/></a>
+          <a href> <TwitterIcon  fontSize = 'large'/></a>
         </div>
-
       </div>
 
       
